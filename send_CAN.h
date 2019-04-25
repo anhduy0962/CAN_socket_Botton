62e2282b97c23cc2e40eeef7f8defadfea1fe326
Nguyen Anh Duy <anhduy0962@gmail.com>
2019-04-25 12:04:23 +0900

    CAN send by botton

---
+++ send_CAN.h
@@ -0,0 +1,90 @@
+#ifndef SEND_CAN_H_
+#define SEND_CAN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+#include <net/if.h>
+#include <sys/ioctl.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+#include "lib.h"
+
+int send_CAN(void)
+{
+	int s; /* can raw socket */ 
+	int nbytes;
+	struct sockaddr_can addr;
+	struct can_frame frame;
+	struct ifreq ifr;
+
+	int i;
+		
+	/* CAN message to be sent out */
+	char buff[] = "7DF#0201050000000000";
+
+	fprintf(stderr,"CAN testing\n");
+	
+	/* parse CAN frame */
+	if (parse_canframe(buff, &frame)){
+		fprintf(stderr, "\nWrong CAN-frame format!\n\n");
+		fprintf(stderr, "Try: <can_id>#{R|data}\n");
+		fprintf(stderr, "can_id can have 3 (SFF) or 8 (EFF) hex chars\n");
+		fprintf(stderr, "data has 0 to 8 hex-values that can (optionally)");
+		fprintf(stderr, " be seperated by '.'\n\n");
+		fprintf(stderr, "e.g. 5A1#11.2233.44556677.88 / 123#DEADBEEF / ");
+		fprintf(stderr, "5AA# /\n     1F334455#1122334455667788 / 123#R ");
+		fprintf(stderr, "for remote transmission request.\n\n");
+		return 1;
+	}
+
+	/* open socket */
+	if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
+		perror("socket");
+		return 1;
+	}
+
+	addr.can_family = AF_CAN;
+
+	strcpy(ifr.ifr_name, "can0");
+	if (ioctl(s, SIOCGIFINDEX, &ifr) < 0) {
+		perror("SIOCGIFINDEX");
+		return 1;
+	}
+	addr.can_ifindex = ifr.ifr_ifindex;
+
+	/* disable default receive filter on this RAW socket */
+	/* This is obsolete as we do not read from the socket at all, but for */
+	/* this reason we can remove the receive list in the Kernel to save a */
+	/* little (really a very little!) CPU usage.                          */
+	setsockopt(s, SOL_CAN_RAW, CAN_RAW_FILTER, NULL, 0);
+
+	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+		perror("bind");
+		return 1;
+	}
+
+	/* send frame */
+	for(i=0;i<100;i++)
+	{
+		if ((nbytes = write(s, &frame, sizeof(frame))) != sizeof(frame)) {
+			perror("write");
+			return 1;
+		}
+		
+		fprintf(stderr, "%d \n", i);
+
+		usleep(10000); /* Delay before next loop */
+	}
+
+	close(s);
+	return 0;
+}
+
+#endif //SEND_CAN_H_
+
